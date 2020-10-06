62fc328d2cc22d453b1a87a1b2552b4a30b0f115
kamalakars04 <kamalakars04@gmail.com>
2020-10-06 11:40:13 +0530

    [Kamalakar].[Refactor].Added UC1_ToValidateFirstName

--- Program.cs
+++ Program.cs
@@ -7,6 +7,21 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To User Registration Problem");
+            //Creating new Validate details instance
+            ValidateDetails validateDetails = new ValidateDetails();
+            while(true)
+            {
+                //Validating first name using ValidateFirstName Method
+                validateDetails.ValidateFirstName();
+                Console.WriteLine("Press Y to validate more details");
+                //If user wants to exit
+                if (Console.ReadLine().ToUpper() != "Y")
+                {
+                    validateDetails.logger.Info("User chose to exit the application");
+                    return;
+                }
+            }
+           
         }
     }
 }
