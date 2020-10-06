c30a845ea96ee6368413dc018d29007c4d76d216
kamalakars04 <70007234+kamalakars04@users.noreply.github.com>
2020-10-06 19:51:34 +0530

    Update UserDetails.cs

--- UserDetails.cs
+++ UserDetails.cs
@@ -15,7 +15,7 @@ namespace UserRegistration
         //constants
         const string patternOfFirstName = "^([A-Z]+)[a-zA-Z]{2,}$";
         const string patternOfLastName = "^([A-Z]+)[a-zA-Z]{2,}$";
-        const string patternOfEmail = "^[a-zA-Z]+([.+_-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})?$";
+        const string patternOfEmail = "^[a-zA-Z0-9]+([.+_-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})?$";
         const string patternOfMobile = "^[0-9]{2}[ ][0-9]{10}$";
         const string patternOfPassword = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[^0-9a-zA-Z])(?!.*[^0-9a-zA-Z].*[^0-9a-zA-Z]).{8,}$";
         //variables
